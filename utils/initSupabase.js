6dd42ce7779b6f4344f458d51e636104ce24a6dd
Joseph Moshood <gladstargtschool6@gmail.com>
2021-07-08 08:50:57 +0100

    Update initSupabase.js

--- utils/initSupabase.js
+++ utils/initSupabase.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  https://imdvvfcbtrfewysxgrnr.supabase.co,
+  https://imdvvfcbtrfewysxgrnr.supabase.co
   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg
 )
 
